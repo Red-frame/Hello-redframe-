07d637ff183888841b7009432afc49c9ca424c9e
Red-frame <135748645+Red-frame@users.noreply.github.com>
2024-02-23 23:42:07 +0700

    Update setup.py

--- helloworld/setup.py
+++ helloworld/setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
-setup(  name= 'helloworld', 
+setup(  name= 'hello-redframe', 
         version='0.0.1', 
         description='Say hello!', 
-        py_modules=["helloworld"],
+        py_modules=["hello-redframe"],
         package_dir={'': 'src'},
         install_requires = ["blessings ~= 1.7"],
         extras_require={
